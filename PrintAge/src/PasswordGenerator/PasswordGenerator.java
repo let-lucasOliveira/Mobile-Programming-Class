0b7b7844edfad79b5ae86efd37cd6e03c193e517
let-lucasOliveira <lucas.nro1000@gmail.com>
2025-08-12 20:11:29 -0300

    Add prototype files.

---
+++ PrintAge/src/PasswordGenerator/PasswordGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PasswordGenerator;
+
+/**
+ *
+ * @author lucas
+ */
+public class PasswordGenerator {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
